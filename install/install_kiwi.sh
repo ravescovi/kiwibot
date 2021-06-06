9824329e194704e9c42f71f8ff5de4ef80da92a1
Ian Foster <foster@anl.gov>
2021-06-05 21:40:03 -0500

    Update install_kiwi.sh

--- install/install_kiwi.sh
+++ install/install_kiwi.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-
-
 shopt -s extglob
 KIWI_WS=~/workspace/kiwibot_ws
 
 rm -rf $KIWI_WS
 mkdir -p $KIWI_WS/src
 
-
 cd $KIWI_WS/src
-git clone https://github.com/ravescovi/locobot
+git clone https://github.com/ravescovi/kiwibot
 
 cd $KIWI_WS
 rosdep install --from-paths src --ignore-src -r -y
@@ -19,4 +16,3 @@ echo "source $KIWI_WS/devel/setup.bash" >> ~/.bashrc
 
 source $KIWI_WS/devel/setup.bash
 shopt -u extglob
-
